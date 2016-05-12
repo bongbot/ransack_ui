667870b6253074740e81c05f1c8d7595aba110a0
Tran Cuong <tcuong1829@gmail.com>
2016-05-12 18:20:31 +0700

    update translate

--- lib/ransack_ui/ransack_overrides/translate.rb
+++ lib/ransack_ui/ransack_overrides/translate.rb
@@ -1,13 +1,16 @@
 module Ransack
   module Translate
     def self.default_interpolation(associated_class)
-      puts "SAIGON"
+      translated_attr = I18n.translate("ransack." + @attr_name)
+      if translated_attr.start_with?("translation missing")
+        puts "TTT:FIELD: " + ("ransack." + @attr_name).inspect
+      end
+
       [
           associated_attribute(associated_class),
           ".attributes.#{@attr_name}".to_sym,
-          I18n.translate(@attr_name)
-      ]
-          .flatten
+          I18n.translate("ransack." + @attr_name)
+      ].flatten
     end
 
   end
